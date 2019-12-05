c6c8969da868d05dfb5211dbea12edf9257731da
Allen Garvey <allengarvey@hotmail.com>
2019-12-04 22:04:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chess Scout
+
+Quickly scout your opponents on lichess.org
+
+## Dependencies
+
+* 
+* 
+
+## Getting Started
+
+* 
+* 
+
+## License
+
+Chess Scout is released under the MIT License. See license.txt for more details.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+            `${__dirname}/src/index.js`, 
+            `${__dirname}/sass/app.scss`,
+        ],
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public/assets'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: 'http://localhost:3000/assets/',
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../assets/app.css',
+        }),
+    ],
+};
