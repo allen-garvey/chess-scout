98520171d213c7dac7053286cf4e7deae013434c
Allen Garvey <allengarvey@hotmail.com>
2020-09-18 22:16:10 -0400

    Update to vue 3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './vue/app.vue'
 
-(function(){
-    new Vue({
-        el: document.getElementById('app'),
-        render: h => h(App),
-    });
-})();
+createApp(App).mount('#app');
