b73d45c9bd09a99f4df97741c6d8ba9c33a8f16e
Allen Garvey <allengarvey@hotmail.com>
2020-10-05 20:47:48 -0400

    Add function to draw black queen

--- src/chess-board/index.js
+++ src/chess-board/index.js
@@ -25,14 +25,14 @@ function drawSquares(context){
 
 function getStartingPostion(){
     return [
-        ['00', '00', '00', '00', '00', '00', '00', '00', ],
-        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', ],
-        ['00', '00', '00', '00', '00', '00', '00', '00', ],
-        ['00', '00', '00', '00', '00', '00', '00', '00', ],
-        ['00', '00', '00', '00', '00', '00', '00', '00', ],
-        ['00', '00', '00', '00', '00', '00', '00', '00', ],
-        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', ],
-        ['wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ', ],
+        ['bQ', 'bQ', 'bQ', 'bQ', 'bQ', 'bQ', 'bQ', 'bQ'],
+        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+        ['00', '00', '00', '00', '00', '00', '00', '00'],
+        ['00', '00', '00', '00', '00', '00', '00', '00'],
+        ['00', '00', '00', '00', '00', '00', '00', '00'],
+        ['00', '00', '00', '00', '00', '00', '00', '00'],
+        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+        ['wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ', 'wQ'],
     ];
 }
 
@@ -43,6 +43,8 @@ function drawPieces(context, board){
         switch(piece){
             case 'wQ':
                 return drawWhiteQueen(context, xPosition, yPosition);
+            case 'bQ':
+                return drawBlackQueen(context, xPosition, yPosition);
             case 'wP':
                 return drawPawn(context, xPosition, yPosition, true);
             case 'bP':
@@ -109,4 +111,54 @@ function drawWhiteQueen(context, x, y){
 
     context.fill(path);
     context.stroke(path);
+}
+
+function drawBlackQueen(context, x, y){
+    const circleSvg = 'M 9 13 A 2 2 0 1 1  5,13 A 2 2 0 1 1  9 13 z';
+    const createQueenCirclePath = (xTranslate, yTranslate) => {
+        const path = new Path2D(circleSvg);
+        const matrix = document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGMatrix();
+        matrix.a = 1;
+        matrix.b = 0;
+        matrix.c = 0;
+        matrix.d = 1;
+        matrix.e = xTranslate;
+        matrix.f = yTranslate;
+        
+        return [path, matrix];
+    }; 
+
+    context.resetTransform();
+    context.translate(x+2, y);
+    context.scale(1.3, 1.3);
+    context.fillStyle = BLACK;
+    context.strokeStyle = BLACK;
+    context.lineWidth = LINE_WIDTH;
+    context.lineCap = 'butt';
+
+    const path2 = new Path2D('M 9,26 C 17.5,24.5 30,24.5 36,26 L 38,14 L 31,25 L 31,11 L 25.5,24.5 L 22.5,9.5 L 19.5,24.5 L 14,10.5 L 14,25 L 7,14 L 9,26 z');
+    context.fill(path2);
+    context.stroke(path2);
+
+    const path3 = new Path2D('M 9,26 C 9,28 10.5,28 11.5,30 C 12.5,31.5 12.5,31 12,33.5 C 10.5,34.5 10.5,36 10.5,36 C 9,37.5 11,38.5 11,38.5 C 17.5,39.5 27.5,39.5 34,38.5 C 34,38.5 35.5,37.5 34,36 C 34,36 34.5,34.5 33,33.5 C 32.5,31 32.5,31.5 33.5,30 C 34.5,28 36,28 36,26 C 27.5,24.5 17.5,24.5 9,26 z');
+    context.fill(path3);
+    context.stroke(path3);
+
+    // circles
+    const path = new Path2D(circleSvg);
+    path.addPath(...createQueenCirclePath(15.5, -5.5));
+    path.addPath(...createQueenCirclePath(32, -1));
+    path.addPath(...createQueenCirclePath(7, -4.5));
+    path.addPath(...createQueenCirclePath(24, -4));
+    
+    context.fill(path);
+    context.stroke(path);
+
+
+    context.stroke(new Path2D('M 11,38.5 A 35,35 1 0 0 34,38.5'));
+    context.strokeStyle = WHITE;
+    context.stroke(new Path2D('M 11,29 A 35,35 1 0 1 34,29'));
+    context.stroke(new Path2D('M 12.5,31.5 L 32.5,31.5'));
+    context.stroke(new Path2D('M 11.5,34.5 A 35,35 1 0 0 33.5,34.5'));
+    context.stroke(new Path2D('M 10.5,37.5 A 35,35 1 0 0 34.5,37.5'));
 }
