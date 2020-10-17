578e6181dcb332e90a9ce01f43b5f3bd99a8bdc6
Allen Garvey <allengarvey@hotmail.com>
2020-10-16 21:20:51 -0400

    Autofocus on search input

--- src/vue/search.vue
+++ src/vue/search.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="search-container">
         <form @submit.prevent="submitAction()">
-            <label>Lichess User Name<input type="search" v-model="userNameInput" /></label>
+            <label>Lichess User Name<input type="search" v-model="userNameInput" v-focus /></label>
             <input type="submit" value="Submit" />
         </form>
     </div>
 </template>
 
 <script>
+import focus from './directives/focus';
+
 export default {
+    directives: {
+        focus,
+    },
     data(){
         return {
             userNameInput: '',
