cea36f15ee0a8231bc9cd54ecd74f3d47d984727
Allen Garvey <allengarvey@hotmail.com>
2021-09-14 10:41:51 -0400

    Add checkboxes on search page for game types

--- src/vue/search.vue
+++ src/vue/search.vue
@@ -2,13 +2,47 @@
     <div class="search-container">
         <form @submit.prevent="submitAction()">
             <label>Lichess User Name<input type="search" v-model="userNameInput" v-focus /></label>
-            <input type="submit" value="Submit" />
+            <div>
+                <template v-for="gameType in gameTypes" :key="gameType.key">
+                    <input :id="gameTypeId(gameType)" type="checkbox" v-model="gameType.isChecked" />
+                    <label :for="gameTypeId(gameType)">{{ gameType.title }}</label>
+                </template>
+            </div>
+            <input type="submit" value="Submit" :disabled="!userNameInput" />
         </form>
     </div>
 </template>
 
 <script>
 import focus from './directives/focus';
+
+const gameTypes = [
+    {
+        title: 'Ultra Bullet',
+        key: 'ultraBullet',
+        isChecked: true,
+    },
+    {
+        title: 'Bullet',
+        key: 'bullet',
+        isChecked: true,
+    },
+    {
+        title: 'Blitz',
+        key: 'blitz',
+        isChecked: true,
+    },
+    {
+        title: 'Rapid',
+        key: 'rapid',
+        isChecked: true,
+    },
+    {
+        title: 'Classical',
+        key: 'classical',
+        isChecked: true,
+    },
+];
 
 export default {
     directives: {
@@ -17,12 +51,20 @@ export default {
     data(){
         return {
             userNameInput: '',
+            gameTypes,
         };
     },
     methods: {
         submitAction(){
             const userName = this.userNameInput;
-            this.$router.push({ name: 'userShow', params: { userName }});
+            const gameTypesQuery = this.gameTypes
+                .filter(gameType => gameType.isChecked)
+                .map(gameType => gameType.key)
+                .join(',');
+            this.$router.push({ name: 'userShow', params: { userName }, query: { gameTypes: gameTypesQuery }});
+        },
+        gameTypeId(gameType){
+            return `${gameType.key}_checkbox_id`;
         },
     }
 };
