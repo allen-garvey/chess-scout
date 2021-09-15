964533386c121d8059de5ec7c1fec7eda084c111
Allen Garvey <allengarvey@hotmail.com>
2021-09-15 09:03:48 -0400

    Fix game types not being added to query param to lichess api

--- src/ajax.js
+++ src/ajax.js
@@ -3,14 +3,14 @@ import { getMoveTrees } from './chess/move-tree';
 
 function getApiUrlForUser(userName, gameTypes){
     const apiUrlBase = 'https://lichess.org/api/games/user/';
-    const queryParamBase = `?max=40&opening=true&perfType=${gameTypes}`;
+    const queryParamBase = `?max=40&perfType=${gameTypes}`;
     // return `${apiUrlBase}${encodeURIComponent(userName)}${queryParamBase}`;
     // for testing so not rate limited by lichess
     return `/assets/${userName}.pgn`;
 }
 
-export function getUserGamesStats(userName){
-    return fetch(getApiUrlForUser(userName)).then((res)=>{
+export function getUserGamesStats(userName, gameTypes){
+    return fetch(getApiUrlForUser(userName, gameTypes)).then((res)=>{
         if(res.status !== 200){
             throw new Error('Error reaching lichess api');
         }
