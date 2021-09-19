d3617f89ad1f5f4d7b50e9ac81caae01ad4805d9
Allen Garvey <allengarvey@hotmail.com>
2021-09-19 10:22:14 -0400

    Add bootstrap styles for search page

--- src/vue/search.vue
+++ src/vue/search.vue
@@ -1,17 +1,38 @@
 <template>
     <div class="container">
         <form @submit.prevent="submitAction()">
-            <label>Lichess User Name<input type="search" v-model="userNameInput" v-focus /></label>
-            <div>
+            <label>Lichess User Name<input type="search" class="form-control" v-model="userNameInput" v-focus /></label>
+            <div :class="$style.checkboxContainer">
                 <template v-for="gameType in gameTypes" :key="gameType.key">
-                    <input :id="gameTypeId(gameType)" type="checkbox" v-model="gameType.isChecked" />
-                    <label :for="gameTypeId(gameType)">{{ gameType.title }}</label>
+                    <input 
+                        :id="gameTypeId(gameType)"
+                        type="checkbox"
+                        class="form-check-input"
+                        v-model="gameType.isChecked" 
+                    />
+                    <label :class="$style.label" :for="gameTypeId(gameType)">{{ gameType.title }}</label>
                 </template>
             </div>
-            <input type="submit" value="Submit" :disabled="!userNameInput" />
+            <div :class="$style.buttonContainer">
+                <input class="btn btn-primary" type="submit" value="Submit" :disabled="!userNameInput" />
+            </div>
         </form>
     </div>
 </template>
+
+<style lang="scss" module>
+    .checkboxContainer {
+        margin: 1rem 0 0.5rem;
+    }
+    .label {
+        cursor: pointer;
+        margin-right: 0.5em;
+    }
+    .buttonContainer {
+        display: flex;
+        justify-content: flex-end;
+    }
+</style>
 
 <script>
 import gameTypes from '../game-types';
