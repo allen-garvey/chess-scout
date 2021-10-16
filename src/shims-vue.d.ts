904140ea676865aabd64c24f6f5567eee67d3afb
Allen Garvey <allengarvey@hotmail.com>
2021-10-16 08:29:01 -0400

    Remove dead code

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,10 +3,4 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     $router: VueRouter
-//   }
-// }
   
