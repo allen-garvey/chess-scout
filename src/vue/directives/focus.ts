62d3bfe5f6539982fb3e3b4081dc5d2c93e99dff
Allen Garvey <allengarvey@hotmail.com>
2021-10-16 08:36:00 -0400

    Convert focus directive to typescript

--- src/vue/directives/focus.ts
+++ src/vue/directives/focus.ts
@@ -1,9 +1,12 @@
+import { Directive } from 'vue';
+
 //from https://stackoverflow.com/questions/34941829/setting-focus-of-an-input-element-in-vue-js
 //usage: <input v-focus>
 
-export default{
-    name: 'focus',
+const focusDirective: Directive =  {
     mounted(el){
         el.focus();
     }
 };
+
+export default focusDirective;
