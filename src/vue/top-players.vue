71c28fcbd08cf5e77220b9e1fc094a7c723530db
Allen Garvey <allengarvey@hotmail.com>
2021-10-30 21:31:46 -0400

    Add styles for top players list

--- src/vue/top-players.vue
+++ src/vue/top-players.vue
@@ -1,21 +1,46 @@
 <template>
     <div :class="$style.container" v-if="topPlayers">
-        <h2>Top Players</h2>
-        <div v-for="gameType in gameTypes" :key="gameType">
-            <h3>{{ gameType }}</h3>
-            <ol>
-                <li v-for="player in topPlayers[gameType]" :key="player.id">
-                    <span :class="$style.playerTitle">{{ player.title ? `${player.title}` : '' }}</span>
-                    <router-link :to="{ name: 'userShow', params: { userName: player.id }, query: { gameTypes: gameType } }">{{ player.username }}</router-link>
-                </li>
-            </ol>
+        <h2 :class="$style.title">Top Players</h2>
+        <div :class="$style.gameTypes">
+            <div 
+                :class="$style.gameType"
+                v-for="gameType in gameTypes" 
+                :key="gameType"
+            >
+                <h3 :class="$style.gameTypeTitle">{{ capitalize(gameType) }}</h3>
+                <ol :class="$style.playerList">
+                    <li v-for="player in topPlayers[gameType]" :key="player.id">
+                        <span :class="$style.playerTitle">{{ player.title ? `${player.title}` : '' }}</span>
+                        <router-link :to="{ name: 'userShow', params: { userName: player.id }, query: { gameTypes: gameType } }">{{ player.username }}</router-link>
+                    </li>
+                </ol>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" module>
 .container {
-
+    margin-top: 1.5rem;
+}
+.title{
+    font-size: 2.05rem;
+    margin: 0 0 1rem;
+}
+.gameTypes {
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 1.5rem;
+}
+.playerList {
+    padding-left: 1em;
+    margin: 0;
+}
+.gameType {
+    margin: 0 2rem 2rem 0;
+}
+.gameTypeTitle {
+    margin: 0 0 0.5rem;
 }
 .playerTitle {
     display: inline-block;
@@ -44,6 +69,9 @@ export default defineComponent({
         },
     },
     methods: {
+        capitalize(s){
+            return s.charAt(0).toUpperCase() + s.slice(1);
+        }
     }
 });
 </script>
